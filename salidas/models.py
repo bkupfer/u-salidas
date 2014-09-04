5b6aa83c4b3b912ec44a4f197cf45078b8326ef0
Bernardo Kupfer <bdokupferb@gmail.com>
2014-09-04 17:26:14 -0400

    [ADD] Boostrap and landing-page

--- salidas/models.py
+++ salidas/models.py
@@ -4,20 +4,36 @@ from django.utils.encoding import smart_text
 # Create your models here.
 class FormData(models.Model):
     # required
-    type_of_commision = models.CharField(max_length=120) # change to list
-    motive = models.TextField(max_length=500)
-    financed_by = models.CharField(max_length=120)
-    amount = models.IntegerField()
-    destiny_country = models.CharField(max_length=120)
-    destiny_city = models.CharField(max_length=120)
-    departure_date = models.DateField()
-    return_date = models.DateField()
+    #type_of_commision = models.CharField(max_length=120) # change to list
+    A = 'Académica'
+    E = 'Estudio'
+    possible_commissions = (
+                    (A, 'Académica'),
+                    (E, 'Estudio'),)
+    type_of_commission = models.CharField(max_length=2,
+                                         name="Tipo de comisión",
+                                         choices=possible_commissions,
+                                         default=A)
+
+    motive = models.TextField(max_length=100, name="Motivo")
+
+    financed = models.CharField(max_length=120, name="Financiamiento")
+
+    currency = models.CharField(max_length=3, name="Tipo de moneda")
+    amount  = models.IntegerField()
+
+    country = models.CharField(max_length=120, name="País de destino")
+    city    = models.CharField(max_length=120, name="Ciudad de destino")
+
+    departure_date  = models.DateTimeField()
+    return_date     = models.DateField()
+
  #   signature = models.ImageField()
-    substitue_techaer = models.CharField(max_length=120)
+    substitute_teacher = models.CharField(max_length=120, name="Profesor substituto")
 
     # aditional info
-    email = models.EmailField()
-    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False) # auto_now_add=True -> when created set time, auto_now=False -> when updated, don't change it
+    email       = models.EmailField()
+    timestamp =  models.DateTimeField(auto_now_add=True, auto_now=False) # auto_now_add=True -> when created set time, auto_now=False -> when updated, don't change it
 
     def __str__(self):
         return smart_text(self.email)
