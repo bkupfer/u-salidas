d5a811db0d81ecd77a1db6c44282f1fc572c606c
catalina <catta.ugs@gmail.com>
2014-10-09 14:18:26 -0300

    Nuevos archivos para las vistas del modulo administrador de solicitudes

--- salidas/views.py
+++ salidas/views.py
@@ -4,6 +4,9 @@ from .new_application import NewApplicationForm
 
 # Create your views here.
 def home(request):
+    return render_to_response("login.html", locals(), context_instance=RequestContext(request))
+
+def index(request):
     return render_to_response("index.html", locals(), context_instance=RequestContext(request))
 
 def new_application(request):
@@ -14,4 +17,16 @@ def new_application(request):
         save_it.save()
 
     return render_to_response("new_application_form.html", locals(), context_instance=RequestContext(request))
+
+def days_validation(request):
+    return render_to_response("days_validation.html", locals(), context_instance=RequestContext(request))
+
+def finance_validation(request):
+    return render_to_response("finance_validation.html", locals(), context_instance=RequestContext(request))
+
+def application_detail(request):
+    return render_to_response("application_detail.html", locals(), context_instance=RequestContext(request))
+
+def replacement_commitment(request):
+    return render_to_response("replacement_commitment.html", locals(), context_instance=RequestContext(request))
 
--- usalidas/urls.py
+++ usalidas/urls.py
@@ -10,7 +10,10 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'salidas.views.home', name='home'),
     url(r'^new_application', 'salidas.views.new_application', name='new_application'),
-
+    url(r'^application_detail', 'salidas.views.application_detail', name='application_detail'),
+    url(r'^days_validation', 'salidas.views.days_validation', name='days_validation'),
+    url(r'^finance_validation', 'salidas.views.finance_validation', name='finance_validation'),
+    url(r'^index', 'salidas.views.index', name='index'),
     # Django's admin site
     url(r'^admin/', include(admin.site.urls)),
 )
