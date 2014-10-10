6a6a0eb99e7ba7574e41cd38772cb8c0b922d6bf
maitegm <maite.gonzalez.mendoza@gmail.com>
2014-10-10 19:22:13 -0300

    Modelos :)

--- salidas/models.py
+++ salidas/models.py
@@ -2,6 +2,75 @@ from django.db import models
 from django.utils.encoding import smart_text
 
 # Create your models here.
+class Currency(models.Model):
+    id = models.AutoField(primary_key=True)
+    currency = models.CharField()
+class Finance_type(models.Model):
+    id = models.AutoField(primary_key=True)
+    type = models.CharField()
+class Finance(models.Model):
+    id_application = models.ForeignKey('Application.id')
+    id_finance_type = models.ForeignKey('Finance.id')
+    id_currency = models.ForeignKey('Currency.id')
+    amount = models.FloatField()
+class Country(model.Model):
+    id = models.AutoField(primary_key=True)
+    country = models.CharField()
+class City(models.Model):
+    id = models.AutoField(primary_key=True)
+    country = models.ForeignKey('Country.id')
+    city = models.CharField()
+class Destination(models.Model):
+    id_Application = models.ForeignKey('Application.id')
+    id_city = models.ForeignKey('City.id')
+    start_date = models.DateField()
+    end_date = models.DateField()
+class Application_state(models.Model):
+    id = models.AutoField(primary_key=True)
+    rut = models.CharField(max_length=10)
+    id_commission_type = models.ForeignKey('Commission_type.type')
+    motive = models.TextField()#blank=True
+    creation_date = models.DateTimeField()
+    id_days_validation_state =  models.ForeignKey('State.id')
+    id_funds_validation_state = models.ForeignKey('State.id')
+    directors_name = models.CharField(max_length=30)
+    directors_rut = models.CharField(max_length=10)
+class Application_state(models.Model):
+    id = models.AutoField(primary_key=True)
+    state = models.CharField(max_length=20)
+class Application_Application_state(models.Model):
+    id_Application = models.ForeignKey('Application.id')
+    id_Application_state = models.ForeignKey('Application_State.id')
+    date =models.DateTimeField()
+class Commission_type(models.Model):
+    id = models.AutoField(primary_key=True)
+    type = models.CharField()
+class Document(models.Model):
+    id = models.AutoField(primary_key=True)
+    path = models.CharField(blank=True, null=True)
+    id_application = models.ForeignKey('Application.id')
+class Teacher(models.Model):
+    rut = models.CharField(max_length=10, primary_key=True)
+    signature_path = models.CharField(max_length=100, blank=True, null=True)
+class Replacement(models.Model):
+    id = models.AutoField(primary_key=True)
+    rut_teacher = models.ForeignKey('Teacher.rut')
+    id_Application = models.ForeignKey('Application.id')
+    #id_curso
+    answer_date = models.DateTimeField(blank=True, null=True)
+    id_state = mdoels.ForeignKey('State.id')
+class State(models.Model):
+    id = models.AutoField(primary_key=True)
+    state = models.CharField(10)
+class inactive_period(models.Model):
+    start_date = models.DateField()
+    end_date = models.DateField()#blank=True, null=True
+    description = models.TextField(blank=True, null=True)
+
+
+
+
+
 class FormData(models.Model):
     # required
     #type_of_commision = models.CharField(max_length=120) # change to list
