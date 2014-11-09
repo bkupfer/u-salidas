dbdfff10041091cf1e9a541e6253519e42e89771
Samirovichs <samiruli@gmail.com>
2014-11-09 18:12:32 -0300

    corregido mini error de identación y comentada linea de south

--- usalidas/settings.py
+++ usalidas/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'south',
+    #'south',
     'salidas',
 )
 
@@ -68,8 +68,8 @@ WSGI_APPLICATION = 'usalidas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
