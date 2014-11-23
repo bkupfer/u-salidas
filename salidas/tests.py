3240c1a40cf7e6683eb4fda7f483c7dce17319c0
Bernardo Kupfer <bdokupferb@gmail.com>
2014-11-23 09:17:35 -0400

    tests

--- salidas/tests.py
+++ salidas/tests.py
@@ -2,3 +2,6 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+    # “If you wish to forget anything on the spot, make a note that this thing is to be remembered.”
+    #― Edgar Allan Poe
